57fee2b5408b2d43a25e46367c121fe0dad739dc
Ceylan Sarıkaya <ceylancyln1453@gmail.com>
2021-03-11 14:41:46 +0300

    Ceylan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'northwind';
+  product: any = { productId: 1, productName: 'Bardak', categoryId: 1, unitePrice: 25 }
+  product1: any = { productId: 2, productName: 'Mause', categoryId: 1, unitePrice: 25 }
+  product2: any = { productId: 3, productName: 'Camera', categoryId: 1, unitePrice: 25 }
+  product3: any = { productId: 4, productName: 'Klavye', categoryId: 1, unitePrice: 25 }
+  product4: any = { productId: 5, productName: 'Kasa', categoryId: 1, unitePrice: 25 }
+
+  products = [this.product1, this.product2, this.product3, this.product4, this.product]
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CategoryComponent } from './category/category.component';
+import { NaviComponent } from './navi/navi.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CategoryComponent,
+    NaviComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
